592bf97e3c2bc5c638a11a53f274d578daeec4c0
Thuong <thuongtrinhduc@tsoft.vn>
2026-05-07 10:58:05 +0700

    feat: welcome page

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -1,3 +1,9 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { ROUTER_PATH } from "./Route";
+import { WelcomePage } from "../pages/auth/pages/WelcomePage";
 
-export const WebRouter = () => <Routes></Routes>;
+export const WebRouter = () => (
+  <Routes>
+    <Route path={ROUTER_PATH.WELCOME} element={<WelcomePage />} />
+  </Routes>
+);
